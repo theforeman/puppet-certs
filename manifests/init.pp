97bac87ea45ea49c070abb5d9e649213fcaf1fe9
Eric D. Helms <ericdhelms@gmail.com>
2013-12-24 18:59:50 -0500

    Parameterizing the module and removing coupling to the Katello module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,16 @@
+# == Class: certs
+#
+# Install and configure certs
+#
+# === Parameters:
+#
+# $oauth_key::              The oauth key for talking to the candlepin API;
+#
 class certs (
+
+  $log_dir = $certs::params::log_dir,
+  $tomcat = $certs::params::tomcat,
+
   $node_fqdn      = $fqdn,
   $generate       = true,
   $regenerate     = false,
@@ -12,23 +24,40 @@ class certs (
   $org_unit       = $certs::params::org_unit,
 
   $expiration     = $certs::params::expiration,
-  $ca_expiration     = $certs::params::ca_expiration
-  ) inherits params {
+  $ca_expiration     = $certs::params::ca_expiration,
+
+  $user_groups = $certs::params::user_groups,
+
+  $pki_dir = $certs::params::pki_dir,
+  $keystore = $certs::params::keystore,
+  $keystore_password_file = $certs::params::keystore_password_file,
+  $keystore_password = $certs::params::keystore_password,
+
+  $candlepin_certs_dir = $certs::params::candlepin_certs_dir,
+  $candlepin_ca_password_file = $certs::params::candlepin_ca_password_file,
+  $ssl_ca_password_file = $certs::params::ssl_ca_password_file,
+
+  $nss_db_password_file = $certs::params::nss_db_password_file,
+  $ssl_pk12_password_file = $certs::params::ssl_pk12_password_file,
+
+  ) inherits certs::params {
+
+  class { 'certs::install': }
 
   $default_ca = Ca['candlepin-ca']
 
   ca { 'candlepin-ca':
     ensure      => present,
     common_name => $certs::ca_common_name,
-    country => $certs::country,
-    state   => $certs::state,
-    city    => $certs::city,
-    org     => $certs::org,
-    org_unit => $certs::org_unit,
-    expiration => $certs::ca_expiration,
-    generate => $certs::generate,
-    regenerate => $certs::regenerate_ca,
-    deploy   => true,
+    country     => $certs::country,
+    state       => $certs::state,
+    city        => $certs::city,
+    org         => $certs::org,
+    org_unit    => $certs::org_unit,
+    expiration  => $certs::ca_expiration,
+    generate    => $certs::generate,
+    regenerate  => $certs::regenerate_ca,
+    deploy      => true,
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,16 @@
+# Certs Parameters
 class certs::params {
-  $ca_common_name = $fqdn  # we need fqdn as CA common name as candlepin uses it as a ssl cert
+
+  if file_exists('/usr/sbin/tomcat') and file_exists('/usr/sbin/tomcat6') {
+    $tomcat = 'tomcat'
+  }
+  else {
+    $tomcat = 'tomcat6'
+  }
+
+  $log_dir = '/var/log/certs'
+
+  $ca_common_name = $::fqdn  # we need fqdn as CA common name as candlepin uses it as a ssl cert
   $country = 'US'
   $state   = 'North Carolina'
   $city    = 'Raleigh'
@@ -7,4 +18,21 @@ class certs::params {
   $org_unit = 'SomeOrgUnit'
   $expiration = '365'
   $ca_expiration = '36500'
+
+  $ssl_ca_password_file = '/etc/katello/ssl_ca_password-file'
+  $candlepin_ca_password_file = '/etc/katello/candlepin_ca_password-file'
+
+  # main keystore location
+  $pki_dir = '/etc/pki/katello'
+  $keystore = "${pki_dir}/keystore"
+  $keystore_password_file = '/etc/katello/keystore_password-file'
+  $keystore_password = find_or_create_password($keystore_password_file)
+
+  $nss_db_password_file = '/etc/katello/nss_db_password-file'
+  $nss_db_dir = '/etc/pki/katello/nssdb'
+  $ssl_pk12_password_file = '/etc/katello/pk12_password-file'
+
+  $candlepin_certs_dir = '/etc/candlepin/certs'
+
+  $user_groups = 'foreman'
 }
