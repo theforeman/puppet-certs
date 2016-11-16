d6effa74b33d1257b120401168fd186778fdb882
Stephen Benjamin <stephen@redhat.com>
2016-11-16 10:57:58 -0500

    refs #17366 - change references from capsule to foreman_proxy_content

--- manifests/tar_create.pp
+++ manifests/tar_create.pp
@@ -1,21 +1,21 @@
-# Definition: certs::tar_create
+# Define: certs::tar_create
 #
-# This class creates a tar ball of generated SSL certs
+# This define creates a tar ball of generated SSL certs
 #
-# Parameters:
-# - The $path of files to tar up
-# - The $capsule_fqdn
+# === Parameters
 #
-# Actions:
-# - Create a tarball of SSL certs
+# $path::                       The $path of files to tar
 #
-# Requires:
-# - The certs class
+# $foreman_proxy_fqdn::         FQDN of the foreman proxy
 #
-define certs::tar_create($path = $title, $capsule_fqdn = $::certs::capsule::capsule_fqdn) {
-  exec { "generate ${path}":
-    cwd     => '/root',
-    path    => ['/usr/bin', '/bin'],
-    command => "tar -czf ${path} ssl-build/*.noarch.rpm ssl-build/${capsule_fqdn}/*.noarch.rpm",
-  }
+define certs::tar_create(
+    $path               = $title,
+    $foreman_proxy_fqdn = $::certs::foreman_proxy_content::foreman_proxy_fqdn,
+) {
+
+    exec { "generate ${path}":
+        cwd     => '/root',
+        path    => ['/usr/bin', '/bin'],
+        command => "tar -czf ${path} ssl-build/*.noarch.rpm ssl-build/${foreman_proxy_fqdn}/*.noarch.rpm",
+    }
 }
