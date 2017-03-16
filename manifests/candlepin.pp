043aeb2db38db9cd339519d73cd78d9eafd111ba
Timo Goebel <mail@timogoebel.name>
2017-03-16 16:49:11 +0100

    fixes #17378 - tomcat has dedicated certificate

--- manifests/candlepin.pp
+++ manifests/candlepin.pp
@@ -8,13 +8,13 @@ class certs::candlepin (
   $deploy                 = $::certs::deploy,
   $ca_cert                = $::certs::ca_cert_stripped,
   $ca_key                 = $::certs::ca_key,
-  $pki_dir                = $::certs::params::pki_dir,
-  $keystore               = $::certs::params::candlepin_keystore,
-  $keystore_password_file = $::certs::params::keystore_password_file,
-  $amqp_truststore        = $::certs::params::candlepin_amqp_truststore,
-  $amqp_keystore          = $::certs::params::candlepin_amqp_keystore,
-  $amqp_store_dir         = $::certs::params::candlepin_amqp_store_dir,
-  $tomcat                 = $::certs::params::tomcat,
+  $pki_dir                = $::certs::pki_dir,
+  $keystore               = $::certs::candlepin_keystore,
+  $keystore_password_file = $::certs::keystore_password_file,
+  $amqp_truststore        = $::certs::candlepin_amqp_truststore,
+  $amqp_keystore          = $::certs::candlepin_amqp_keystore,
+  $amqp_store_dir         = $::certs::candlepin_amqp_store_dir,
+  $tomcat                 = $::certs::tomcat,
 ) inherits certs::params {
 
   Exec {
@@ -41,14 +41,42 @@ class certs::candlepin (
     password_file => $::certs::ca_key_password_file,
   }
 
+  $tomcat_cert_name = "${hostname}-tomcat"
+  $tomcat_cert = "${pki_dir}/certs/katello-tomcat.crt"
+  $tomcat_key  = "${pki_dir}/private/katello-tomcat.key"
+
+  cert { $tomcat_cert_name:
+    ensure        => present,
+    hostname      => $hostname,
+    cname         => $cname,
+    country       => $::certs::country,
+    state         => $::certs::state,
+    city          => $::certs::city,
+    org           => $::certs::org,
+    org_unit      => $::certs::org_unit,
+    expiration    => $::certs::expiration,
+    ca            => $::certs::default_ca,
+    generate      => $generate,
+    regenerate    => $regenerate,
+    deploy        => $deploy,
+    password_file => $::certs::ca_key_password_file,
+  }
+
   $keystore_password = cache_data('foreman_cache_data', $keystore_password_file, random_password(32))
-  $password_file = "${::certs::pki_dir}/keystore_password-file"
-  $client_req = "${::certs::pki_dir}/java-client.req"
-  $client_cert = "${::certs::pki_dir}/certs/${java_client_cert_name}.crt"
-  $client_key = "${::certs::pki_dir}/private/${java_client_cert_name}.key"
+  $password_file = "${pki_dir}/keystore_password-file"
+  $client_req = "${pki_dir}/java-client.req"
+  $client_cert = "${pki_dir}/certs/${java_client_cert_name}.crt"
+  $client_key = "${pki_dir}/private/${java_client_cert_name}.key"
 
   if $deploy {
 
+    Cert[$tomcat_cert_name] ~>
+    pubkey { $tomcat_cert:
+      key_pair => Cert[$tomcat_cert_name],
+    } ~>
+    privkey { $tomcat_key:
+      key_pair => Cert[$tomcat_cert_name],
+    } ~>
     file { $password_file:
       ensure  => file,
       content => $keystore_password,
@@ -57,7 +85,7 @@ class certs::candlepin (
       mode    => '0440',
     } ~>
     exec { 'candlepin-generate-ssl-keystore':
-      command => "openssl pkcs12 -export -in ${ca_cert} -inkey ${ca_key} -out ${keystore} -name tomcat -CAfile ${ca_cert} -caname root -password \"file:${password_file}\" -passin \"file:${::certs::ca_key_password_file}\" ",
+      command => "openssl pkcs12 -export -in ${tomcat_cert} -inkey ${tomcat_key} -out ${keystore} -name tomcat -CAfile ${ca_cert} -caname root -password \"file:${password_file}\" -passin \"file:${::certs::ca_key_password_file}\" ",
       creates => $keystore,
     } ~>
     file { "/usr/share/${tomcat}/conf/keystore":
