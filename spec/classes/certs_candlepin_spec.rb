27d497c82ca62b71473e7d5b0c0ce90ded6f9de3
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-04 14:57:34 +0200

    Remove unused pre_condition

--- spec/classes/certs_candlepin_spec.rb
+++ spec/classes/certs_candlepin_spec.rb
@@ -5,10 +5,6 @@ describe 'certs::candlepin' do
     on_supported_os['redhat-7-x86_64']
   end
 
-  let :pre_condition do
-    "service{'qpidd': }"
-  end
-
   describe 'with default parameters' do
     it { should compile.with_all_deps }
   end
