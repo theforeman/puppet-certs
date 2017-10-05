72ba9783d4701aa9744a25b1289b07ad3dbff11a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-05 12:41:21 +0200

    Use implicit dependency chaining on puppet
    https://github.com/theforeman/puppet-puppet/commit/2fb8ad506566092d146492f49e7892ca0fb93b64
    ensures the puppet user is always managed. This means the file resource
    will autorequire the user causing the chaining to work.

--- spec/classes/certs_puppet_spec.rb
+++ spec/classes/certs_puppet_spec.rb
@@ -5,16 +5,6 @@ describe 'certs::puppet' do
     on_supported_os['redhat-7-x86_64']
   end
 
-  let :pre_condition do
-    "
-class { 'puppet':
-  server_foreman => false,
-  agent => false,
-  server => true,
-}
-    "
-  end
-
   describe 'with default parameters' do
     it { should compile.with_all_deps }
   end
