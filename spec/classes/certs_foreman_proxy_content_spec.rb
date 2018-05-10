b8367b13135445bcf3dd8b830f305e5df3ad5d75
Jonathon Turel <jturel@redhat.com>
2018-05-10 12:18:03 -0400

    Remove dependency on puppet-common

--- spec/classes/certs_foreman_proxy_content_spec.rb
+++ spec/classes/certs_foreman_proxy_content_spec.rb
@@ -7,12 +7,20 @@ describe 'certs::foreman_proxy_content' do
 
   let :params do
     {
-      :certs_tar => '/tmp/tar',
-      :foreman_proxy_fqdn => 'bar.example.com'
+      certs_tar: '/tmp/tar',
+      foreman_proxy_fqdn: 'bar.example.com'
     }
   end
 
   describe 'with default parameters' do
     it { should compile.with_all_deps }
+  end
+
+  context 'with empty certs_tar' do
+    let(:params) { { certs_tar: '' } }
+
+    it do
+      should compile.and_raise_error(/\'certs_tar\' expects a String\[1/)
+    end
   end
 end
