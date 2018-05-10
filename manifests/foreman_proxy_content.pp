ae88afeb6c40dcb1b3c0ad66f5636784c4d77e8e
Jonathon Turel <jturel@redhat.com>
2018-05-10 12:18:03 -0400

    Remove dependency on puppet-common

--- manifests/foreman_proxy_content.pp
+++ manifests/foreman_proxy_content.pp
@@ -12,16 +12,11 @@
 # $certs_tar::                      Path to tar file with certs to generate
 #
 class certs::foreman_proxy_content (
-  Optional[String] $parent_fqdn = $::fqdn,
+  String[1] $parent_fqdn = $::fqdn,
   String $foreman_proxy_fqdn = $::certs::params::node_fqdn,
   Array[String] $foreman_proxy_cname = $::certs::params::cname,
-  Optional[String] $certs_tar = $::certs::params::certs_tar,
+  String[1] $certs_tar = $::certs::params::certs_tar,
 ) inherits certs::params {
-
-  # until we support again pushing the cert rpms to the Katello,
-  # make sure the certs_tar path is present
-  validate_present($certs_tar)
-  validate_present($foreman_proxy_fqdn)
 
   if $foreman_proxy_fqdn == $::fqdn {
     fail('The hostname is the same as the provided hostname for the foreman-proxy')
@@ -35,9 +30,7 @@ class certs::foreman_proxy_content (
   class { '::certs::qpid_router':   hostname => $foreman_proxy_fqdn, cname => $foreman_proxy_cname }
   class { '::certs::qpid_client':   hostname => $foreman_proxy_fqdn, cname => $foreman_proxy_cname }
 
-  if $certs_tar {
-    certs::tar_create { $certs_tar:
-      subscribe => Class['certs::puppet', 'certs::foreman', 'certs::foreman_proxy', 'certs::qpid', 'certs::qpid_router', 'certs::apache', 'certs::qpid_client'],
-    }
+  certs::tar_create { $certs_tar:
+    subscribe => Class['certs::puppet', 'certs::foreman', 'certs::foreman_proxy', 'certs::qpid', 'certs::qpid_router', 'certs::apache', 'certs::qpid_client'],
   }
 }
