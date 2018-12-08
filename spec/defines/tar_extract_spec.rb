227de18cde6455aab156ff55a283b270bb7715be
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-12-09 00:00:36 +0100

    Use better rspec spies testing
    This is the preferred pattern of setting up allows, running the code and
    then checking the expectations.

--- spec/defines/tar_extract_spec.rb
+++ spec/defines/tar_extract_spec.rb
@@ -6,12 +6,15 @@ describe 'certs::tar_extract' do
   context 'without parameters' do
     it do
       allow(File).to receive(:exist?).and_call_original
-      expect(File).to receive(:exist?).with('/path/to/certs.tar').and_return(true)
+      allow(File).to receive(:exist?).with('/path/to/certs.tar').and_return(true)
+
       is_expected.to compile.with_all_deps
       is_expected.to contain_exec('extract /path/to/certs.tar')
         .with_cwd('/root')
         .with_path(['/usr/bin', '/bin'])
         .with_command('tar -xaf /path/to/certs.tar')
+
+      expect(File).to have_received(:exist?).with('/path/to/certs.tar')
     end
   end
 
@@ -20,13 +23,16 @@ describe 'certs::tar_extract' do
 
     it do
       allow(File).to receive(:exist?).and_call_original
-      expect(File).not_to receive(:exist?).with('/path/to/certs.tar')
-      expect(File).to receive(:exist?).with('/some/other/path/with/certs.tar').and_return(true)
+      allow(File).to receive(:exist?).with('/some/other/path/with/certs.tar').and_return(true)
+
       is_expected.to compile.with_all_deps
       is_expected.to contain_exec('extract /some/other/path/with/certs.tar')
         .with_cwd('/root')
         .with_path(['/usr/bin', '/bin'])
         .with_command('tar -xaf /some/other/path/with/certs.tar')
+
+      expect(File).not_to have_received(:exist?).with('/path/to/certs.tar')
+      expect(File).to have_received(:exist?).with('/some/other/path/with/certs.tar')
     end
   end
 end
