2d14f54b00769ddf86adb867fa94cd99ede697a2
treydock <treydock@gmail.com>
2019-03-05 12:23:20 -0500

    Fixes #26180 - Fix puppet generate types by moving common
    Move type common module to PuppetX namespace to avoid breaking `puppet generate types`.

--- lib/puppet/type/pubkey.rb
+++ lib/puppet/type/pubkey.rb
@@ -1,9 +1,9 @@
-require File.expand_path('../certs_common', __FILE__)
+require_relative '../../puppet_x/certs/common'
 
 Puppet::Type.newtype(:pubkey) do
   desc 'Stores the public key file in a location'
 
-  instance_eval(&Certs::FILE_COMMON_PARAMS)
+  instance_eval(&PuppetX::Certs::Common::FILE_COMMON_PARAMS)
 
   # will generate a key with the certificate information stripped
   newparam(:strip)
