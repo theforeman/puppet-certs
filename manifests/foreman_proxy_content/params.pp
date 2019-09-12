3beda1df35481210b581f9beeb63d1dcafd2aca0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-09-12 19:01:55 +0200

    Fixes #27847 - Refactor foreman_proxy_content class
    * Move $parent_fqdn to Advanced Parameters since users generally don't
      touch this
    * Implement stricter data types
    * Use modern facts
    * Work around a kafo_parsers/puppet-strings limitation by using
      class inheritance

---
+++ manifests/foreman_proxy_content/params.pp
@@ -0,0 +1,11 @@
+# @summary Defaults for certs::foreman_proxy_content::params
+# @api private
+#
+# This class exists because puppet-strings generates a string "[]" rather than
+# an empty array in json for defaults. Kafo also can't handle the hash $facts
+# even though it's the current recommended default. By adding indirection to a
+# class we can work around this.
+class certs::foreman_proxy_content::params {
+  $parent_fqdn = $facts['networking']['fqdn']
+  $foreman_proxy_cname = []
+}
