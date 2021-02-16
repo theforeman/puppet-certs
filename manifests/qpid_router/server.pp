fc52cbcc9681bc5f18de3e81ef41bdf44693acaf
Eric D. Helms <ericdhelms@gmail.com>
2021-02-16 15:33:57 -0500

    Refs #31878 - Split qpid router server and client certificates

--- manifests/qpid_router/server.pp
+++ manifests/qpid_router/server.pp
@@ -1,14 +1,12 @@
 # Constains certs specific configurations for qpid dispatch router
-class certs::qpid_router (
+class certs::qpid_router::server (
   $hostname               = $certs::node_fqdn,
   $cname                  = $certs::cname,
   $generate               = $certs::generate,
   $regenerate             = $certs::regenerate,
   $deploy                 = $certs::deploy,
-  $server_cert            = $certs::qpid_router_server_cert,
-  $client_cert            = $certs::qpid_router_client_cert,
-  $server_key             = $certs::qpid_router_server_key,
-  $client_key             = $certs::qpid_router_client_key,
+  $cert                   = $certs::qpid_router_server_cert,
+  $key                    = $certs::qpid_router_server_key,
   $owner                  = 'qdrouterd',
   $group                  = 'root',
 
@@ -22,7 +20,6 @@ class certs::qpid_router (
 ) inherits certs {
 
   $server_keypair = "${hostname}-qpid-router-server"
-  $client_keypair = "${hostname}-qpid-router-client"
 
   cert { $server_keypair:
     ensure        => present,
@@ -42,47 +39,15 @@ class certs::qpid_router (
     password_file => $ca_key_password_file,
   }
 
-  cert { $client_keypair:
-    ensure        => present,
-    hostname      => $hostname,
-    cname         => $cname,
-    country       => $country,
-    state         => $state,
-    city          => $city,
-    org           => 'dispatch client',
-    org_unit      => $org_unit,
-    expiration    => $expiration,
-    ca            => $default_ca,
-    generate      => $generate,
-    regenerate    => $regenerate,
-    deploy        => $deploy,
-    purpose       => 'client',
-    password_file => $ca_key_password_file,
-  }
-
   if $deploy {
-    certs::keypair { 'qpid_router_server':
+    certs::keypair { 'qpid_dispatch_server':
       key_pair    => Cert[$server_keypair],
-      key_file    => $server_key,
+      key_file    => $key,
       manage_key  => true,
       key_owner   => $owner,
       key_group   => $group,
       key_mode    => '0640',
-      cert_file   => $server_cert,
-      manage_cert => true,
-      cert_owner  => $owner,
-      cert_group  => $group,
-      cert_mode   => '0640',
-    }
-
-    certs::keypair { 'qpid_router_client':
-      key_pair    => Cert[$client_keypair],
-      key_file    => $client_key,
-      manage_key  => true,
-      key_owner   => $owner,
-      key_group   => $group,
-      key_mode    => '0640',
-      cert_file   => $client_cert,
+      cert_file   => $cert,
       manage_cert => true,
       cert_owner  => $owner,
       cert_group  => $group,
