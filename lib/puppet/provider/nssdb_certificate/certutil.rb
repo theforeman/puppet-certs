96c6e9b64d52766f354866e278612a20a6055afa
Eric D. Helms <ericdhelms@gmail.com>
2021-05-03 09:15:47 -0400

    Fixes #32506: Add an nssdb_certificate type and provider
    This adds a type for managing NSS database certificates (and private keys)
    within an NSS database. This new type reduces the number of intermediary
    entities that have to be created, for example the pkcs12 export, and
    reduces the overall Puppet code required. This type aims to provide
    a clean interface to managing NSS database certificates and ensure
    that the NSS database is in sync with the certificates being managed.

---
+++ lib/puppet/provider/nssdb_certificate/certutil.rb
@@ -0,0 +1,117 @@
+Puppet::Type.type(:nssdb_certificate).provide(:certutil) do
+  commands :certutil => 'certutil'
+  commands :openssl => 'openssl'
+  commands :pk12util => 'pk12util'
+
+  def create
+    add_certificate
+    add_private_key if resource[:private_key]
+  end
+
+  def destroy
+    delete_certificate
+    delete_private_key if resource[:private_key]
+  end
+
+  def exists?
+    nssdb_content
+  end
+
+  def certificate
+    nssdb_fingerprint
+  end
+
+  def certificate=(value)
+    delete_certificate unless nssdb_content.nil?
+    add_certificate
+  end
+
+  def fingerprint(file)
+    return unless File.exist?(file)
+
+    openssl('x509', '-sha256', '-noout', '-fingerprint', '-in', file).strip.split('=')[1]
+  rescue Puppet::ExecutionFailure => e
+    Puppet.warn("Failed to read certificate #{file}: #{e}")
+    nil
+  end
+
+  private
+
+  def add_certificate
+    certutil(
+      '-A',
+      '-a',
+      '-d', resource[:nssdb],
+      '-n', resource[:cert_name],
+      '-t', resource[:trustargs],
+      '-i', resource[:certificate],
+      '-f', resource[:password_file]
+    )
+  end
+
+  def delete_certificate
+    certutil(
+      '-D',
+      '-d', resource[:nssdb],
+      '-n', resource[:cert_name]
+    )
+  end
+
+  def add_private_key
+    Tempfile.open('pkcs12') do |pkcs12|
+      openssl(
+        'pkcs12',
+        '-export',
+        '-in', resource[:certificate],
+        '-inkey', resource[:private_key],
+        '-out', pkcs12.path,
+        '-password', "file:#{resource[:password_file]}",
+        '-name', resource[:cert_name]
+      )
+
+      pk12util(
+        '-i', pkcs12.path,
+        '-d', resource[:nssdb],
+        '-w', resource[:password_file],
+        '-k', resource[:password_file]
+      )
+    end
+  end
+
+  def delete_private_key
+    certutil(
+      '-F',
+      '-d', resource[:nssdb],
+      '-n', resource[:cert_name]
+    )
+  end
+
+  def nssdb_content
+    return unless directory_readable?(resource[:nssdb])
+
+    certutil(
+      '-L',
+      '-a',
+      '-d', resource[:nssdb],
+      '-n', resource[:cert_name]
+    )
+  rescue Puppet::ExecutionFailure => e
+    Puppet.debug("Failed to read nssdb contents from #{resource[:nssdb]}: #{e}")
+    nil
+  end
+
+  def nssdb_fingerprint
+    cert_info = nssdb_content
+    return unless cert_info
+
+    Tempfile.open('cert') do |temp_cert|
+      temp_cert.write(cert_info)
+      temp_cert.rewind
+      fingerprint(temp_cert.path)
+    end
+  end
+
+  def directory_readable?(file)
+    File.directory?(file) && File.readable?(file)
+  end
+end
