5516509288ca0329c84a695a2e2f68016fdfb41e
Eric D. Helms <ericdhelms@gmail.com>
2021-06-23 21:17:43 -0400

    Add acceptance test for truststore_certificate

--- spec/acceptance/truststore_spec.rb
+++ spec/acceptance/truststore_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper_acceptance'
 
 describe 'certs' do
+  truststore_password_file = '/etc/pki/truststore_password-file'
+  truststore_path = '/etc/pki/truststore'
+
   context 'with truststore type' do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
@@ -20,7 +23,7 @@ describe 'certs' do
           show_diff => false,
         }
 
-        truststore { "/etc/pki/truststore":
+        truststore { "#{truststore_path}":
           ensure        => present,
           password_file => $truststore_password_file,
           owner         => 'root',
@@ -31,17 +34,58 @@ describe 'certs' do
       end
     end
 
-    describe file('/etc/pki/truststore') do
+    describe file(truststore_path) do
       it { should be_file }
       it { should be_mode 640 }
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
     end
 
-    describe command("keytool -list -keystore /etc/pki/truststore -storepass:file /etc/pki/truststore_password-file") do
+    describe command("keytool -list -keystore #{truststore_path} -storepass:file #{truststore_password_file}") do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should match(/^Keystore type: PKCS12$/i) }
       its(:stdout) { should match(/^Your keystore contains 0 entries$/) }
+    end
+  end
+
+  context 'with a truststore_certificate' do
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+        $truststore_password_file = '#{truststore_password_file}'
+
+        $ca_key = '/etc/pki/ca.key'
+        $ca_cert = '/etc/pki/ca.crt'
+
+        exec { 'Create CA key':
+          command => "/usr/bin/openssl genrsa -out '${ca_key}' 2048",
+          creates => $ca_key,
+        } ->
+        exec { 'Create CA certficate':
+          command => "/usr/bin/openssl req -new -x509 -key '${ca_key}' -out '${ca_cert}' -nodes -x509 -subj '/CN=${facts['networking']['fqdn']}'",
+          creates => $ca_cert,
+        }
+
+        truststore_certificate { "#{truststore_path}:fake-ca":
+          ensure        => present,
+          password_file => $truststore_password_file,
+          certificate   => $ca_cert,
+        }
+        PUPPET
+      end
+    end
+
+    describe command("keytool -list -keystore #{truststore_path} -storepass $(cat #{truststore_password_file})") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match(/^Keystore type: PKCS12$/i) }
+      its(:stdout) { should match(/^Your keystore contains 1 entry$/) }
+      its(:stdout) { should match(/^fake-ca, .+, trustedCertEntry, $/) }
+    end
+
+    describe command("keytool -list -v -keystore #{truststore_path} -alias fake-ca -storepass $(cat #{truststore_password_file})") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match(/^Owner: CN=#{host_inventory['fqdn']}$/) }
+      its(:stdout) { should match(/^Issuer: CN=#{host_inventory['fqdn']}$/) }
     end
   end
 end
