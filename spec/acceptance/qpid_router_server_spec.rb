c8695f307ee08ce4f65b01e3280df9855c32b933
Eric D. Helms <ericdhelms@gmail.com>
2021-07-27 22:14:56 -0400

    Deploy qpid-dispatch certificates from build directory

--- spec/acceptance/qpid_router_server_spec.rb
+++ spec/acceptance/qpid_router_server_spec.rb
@@ -44,7 +44,7 @@ describe 'certs::qpid_router::server' do
 
     describe file('/etc/pki/katello/qpid_router_server.key') do
       it { should be_file }
-      it { should be_mode 640 }
+      it { should be_mode 440 }
       it { should be_owned_by 'qdrouterd' }
       it { should be_grouped_into 'root' }
     end
@@ -62,6 +62,53 @@ describe 'certs::qpid_router::server' do
       it { should_not be_encrypted }
       it { should be_valid }
       it { should have_matching_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-qpid-router-server.crt") }
+    end
+
+    describe package("#{fact('fqdn')}-qpid-router-server") do
+      it { should_not be_installed }
+    end
+  end
+
+  context 'with deploy false' do
+    before(:context) do
+      on default, 'rm -rf /root/ssl-build /etc/pki/katello'
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+          class { 'certs::qpid_router::server':
+            deploy => false
+          }
+        PUPPET
+      end
+    end
+
+    describe x509_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-qpid-router-server.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'server' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = dispatch server, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-qpid-router-server.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-qpid-router-server.crt") }
+    end
+
+    describe file('/etc/pki/katello/certs/qpid_router_server.crt') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/pki/katello/private/qpid_router_server.key') do
+      it { should_not exist }
+    end
+
+    describe package("#{fact('fqdn')}-qpid-router-server") do
+      it { should_not be_installed }
     end
   end
 end
