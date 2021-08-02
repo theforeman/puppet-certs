52b54758eabf35a386b862888dda36d6c159e980
Eric D. Helms <ericdhelms@gmail.com>
2021-08-02 08:50:48 -0400

    Deploy Foreman proxy certificates from build directory

--- spec/acceptance/foreman_proxy_spec.rb
+++ spec/acceptance/foreman_proxy_spec.rb
@@ -100,12 +100,34 @@ describe 'certs::foreman_proxy' do
       its(:keylength) { should be >= 4096 }
     end
 
-    describe file("/etc/pki/katello/private/#{fqdn}-foreman-proxy-client-bundle.pem") do
-      it { should be_file }
-      it { should be_mode 444 }
-      it { should be_owned_by 'root' }
-      it { should be_grouped_into 'foreman-proxy' }
-      its(:content) { should include('BEGIN RSA PRIVATE KEY') }
+    describe x509_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'server' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fqdn}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = FOREMAN, OU = SMART_PROXY, CN = #{fqdn}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.crt") }
+    end
+
+    describe x509_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'client' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fqdn}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = FOREMAN, OU = FOREMAN_PROXY, CN = #{fqdn}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.crt") }
     end
 
     describe x509_certificate("/etc/pki/katello/private/#{fqdn}-foreman-proxy-client-bundle.pem") do
@@ -123,5 +145,94 @@ describe 'certs::foreman_proxy' do
       it { should have_matching_certificate("/etc/pki/katello/private/#{fqdn}-foreman-proxy-client-bundle.pem") }
     end
 
+    describe file("/etc/pki/katello/private/#{fqdn}-foreman-proxy-client-bundle.pem") do
+      it { should be_file }
+      it { should be_mode 444 }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'foreman-proxy' }
+      its(:content) { should include('BEGIN RSA PRIVATE KEY') }
+    end
+
+    describe package("#{fact('fqdn')}-foreman-proxy") do
+      it { should_not be_installed }
+    end
+
+    describe package("#{fact('fqdn')}-foreman-proxy-client") do
+      it { should_not be_installed }
+    end
+  end
+
+  context 'with deploy false' do
+    before(:context) do
+      on default, 'rm -rf /root/ssl-build /etc/foreman-proxy'
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+          class { 'certs::foreman_proxy':
+            deploy => false
+          }
+        PUPPET
+      end
+    end
+
+    describe x509_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'server' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fqdn}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = FOREMAN, OU = SMART_PROXY, CN = #{fqdn}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy.crt") }
+    end
+
+    describe x509_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'client' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fqdn}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = FOREMAN, OU = FOREMAN_PROXY, CN = #{fqdn}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fqdn}/#{fqdn}-foreman-proxy-client.crt") }
+    end
+
+    describe file('/etc/foreman-proxy/ssl_cert.pem') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/foreman-proxy/ssl_key.pem') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/foreman-proxy/ssl_ca.pem') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/foreman-proxy/foreman_ssl_cert.pem') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/foreman-proxy/foreman_ssl_key.pem') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/foreman-proxy/foreman_ssl_ca.pem') do
+      it { should_not exist }
+    end
+
+    describe file("/etc/pki/katello/private/#{fqdn}/#{fqdn}-foreman-proxy-client-bundle.pem") do
+      it { should_not exist }
+    end
   end
 end
