theforeman/puppet-certs

commit 8c5404ff76aca156be16f81b1f21b630a067b2c2

Deploy Apache certificates from build directory
This introduces a new wrapper object key_pair for deploying and
managing a certificate and private key based on input files. This change
skips the creation and installation of RPMs, instead opting to deploy
the key and certificate directly from the build directory. This skips
a lot of overhead that comes with the RPMs.

manifests/apache.pp
@@ -23,7 +23,6 @@ class certs::apache (
   $apache_cert_name = "${hostname}-apache"
   $apache_cert = "${pki_dir}/certs/katello-apache.crt"
   $apache_key  = "${pki_dir}/private/katello-apache.key"
-  $apache_ca_cert = $certs::katello_server_ca_cert
 
   if $server_cert {
     cert { $apache_cert_name:
@@ -31,7 +30,7 @@ class certs::apache (
       hostname       => $hostname,
       cname          => $cname,
       generate       => $generate,
-      deploy         => $deploy,
+      deploy         => false,
       regenerate     => $regenerate,
       custom_pubkey  => $server_cert,
       custom_privkey => $server_key,
@@ -52,21 +51,24 @@ class certs::apache (
       ca            => $default_ca,
       generate      => $generate,
       regenerate    => $regenerate,
-      deploy        => $deploy,
+      deploy        => false,
       password_file => $ca_key_password_file,
       build_dir     => $certs::ssl_build_dir,
     }
   }
 
   if $deploy {
-    certs::keypair { 'apache':
-      key_pair   => Cert[$apache_cert_name],
+    certs::key_pair { $apache_cert_name:
+      source_dir => "${certs::ssl_build_dir}/${hostname}",
       key_file   => $apache_key,
-      manage_key => true,
       key_owner  => 'root',
       key_group  => $group,
       key_mode   => '0440',
       cert_file  => $apache_cert,
+      cert_owner => 'root',
+      cert_group => $group,
+      cert_mode  => '0440',
+      require    => Cert[$apache_cert_name],
     }
   }
 }

manifests/key_pair.pp
@@ -1,0 +1,34 @@
+# @summary Deploy a certificate and its matching key
+# @api private
+define certs::key_pair (
+  Stdlib::Absolutepath $source_dir,
+  Stdlib::Absolutepath $key_file,
+  Stdlib::Absolutepath $cert_file,
+  Enum['present', 'absent'] $key_ensure = 'present',
+  String $key_owner = 'root',
+  String $key_group = undef,
+  Stdlib::Filemode $key_mode = '440',
+  Enum['present', 'absent'] $cert_ensure = 'present',
+  String $cert_owner = 'root',
+  String $cert_group = undef,
+  Stdlib::Filemode $cert_mode = '440',
+) {
+
+  file { $key_file:
+    ensure    => $key_ensure,
+    source    => "${source_dir}/${title}.key",
+    owner     => $key_owner,
+    group     => $key_group,
+    mode      => $key_mode,
+    show_diff => false,
+  }
+
+  file { $cert_file:
+    ensure => $cert_ensure,
+    source => "${source_dir}/${title}.crt",
+    owner  => $cert_owner,
+    group  => $cert_group,
+    mode   => $cert_mode,
+  }
+
+}

spec/acceptance/apache_spec.rb
@@ -30,8 +30,23 @@ describe 'certs::apache' do
       it { should have_matching_certificate('/etc/pki/katello/certs/katello-apache.crt') }
     end
 
+    describe x509_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'server' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.crt") }
+    end
+
     describe package("#{fact('fqdn')}-apache") do
-      it { should be_installed }
+      it { should_not be_installed }
     end
   end
 
@@ -74,7 +89,46 @@ describe 'certs::apache' do
     end
 
     describe package("#{fact('fqdn')}-apache") do
-      it { should be_installed }
+      it { should_not be_installed }
+    end
+  end
+
+  context 'with deploy false' do
+    before(:context) do
+      on default, 'rm -rf /root/ssl-build /etc/pki/katello'
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+          class { 'certs::apache':
+            deploy => false
+          }
+        PUPPET
+      end
+    end
+
+    describe x509_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.crt") do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'server' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.key") do
+      it { should_not be_encrypted }
+      it { should be_valid }
+      it { should have_matching_certificate("/root/ssl-build/#{fact('fqdn')}/#{fact('fqdn')}-apache.crt") }
+    end
+
+    describe file('/etc/pki/katello/certs/katello-apache.crt') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/pki/katello/private/katello-apache.key') do
+      it { should_not exist }
     end
   end
 end

spec/acceptance/certs_tar_extract_spec.rb
@@ -72,7 +72,7 @@ describe 'certs with tar archive' do
   end
 
   describe package("foreman-proxy.example.com-apache") do
-    it { should be_installed }
+    it { should_not be_installed }
   end
 
   describe file('/root/ssl-build/foreman-proxy.example.com') do

spec/classes/certs_apache_spec.rb
@@ -19,7 +19,7 @@ describe 'certs::apache' do
         it { should compile.with_all_deps }
 
         it do
-          is_expected.to contain_certs__keypair('apache')
+          is_expected.to contain_certs__key_pair("#{facts[:fqdn]}-apache")
             .with_key_group('foreman')
         end
       end

commit 711fe144a6f6c9914d5e55483488e8279b882fdc

Reintroduce $apache_ca_cert
In 8c5404ff76aca156be16f81b1f21b630a067b2c2 this was removed, but it was
considered a public API. It makes it easy for consumers to know what the
actual CA is.

Fixes: 8c5404ff76aca156be16f81b1f21b630a067b2c2

manifests/apache.pp
@@ -23,6 +23,8 @@ class certs::apache (
   $apache_cert_name = "${hostname}-apache"
   $apache_cert = "${pki_dir}/certs/katello-apache.crt"
   $apache_key  = "${pki_dir}/private/katello-apache.key"
+  # This variable is unused but considered public API
+  $apache_ca_cert = $certs::katello_server_ca_cert
 
   if $server_cert {
     cert { $apache_cert_name: