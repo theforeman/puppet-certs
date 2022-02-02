37d3b3c967572b89ab331a42044787bf5347ea5f
Evgeni Golov <evgeni@golov.de>
2022-02-02 14:32:31 +0100

    use foreman::repo to setup foreman repos during CI

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,12 +1,5 @@
-$major = $facts['os']['release']['major']
-
-# Defaults to staging, for release, use
-# $baseurl = "https://yum.theforeman.org/releases/nightly/el${major}/x86_64/"
-$baseurl = "http://koji.katello.org/releases/yum/foreman-nightly/RHEL/${major}/x86_64/"
-
-yumrepo { 'foreman':
-  baseurl  => $baseurl,
-  gpgcheck => 0,
+class { 'foreman::repo':
+  repo => 'nightly',
 }
 
 group { 'pulp':
