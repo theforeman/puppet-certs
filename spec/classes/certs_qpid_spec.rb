8d86637fa5af828e3aaa26729001f0439642dbc1
Eric D. Helms <ericdhelms@gmail.com>
2022-06-08 21:56:55 -0400

    Drop EL7 support

--- spec/classes/certs_qpid_spec.rb
+++ spec/classes/certs_qpid_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'certs::qpid' do
   on_supported_os.each do |os, os_facts|
-    context "on #{os}", if: os_facts[:operatingsystemmajrelease] == '7' do
+    context "on #{os}" do
       let :facts do
         os_facts
       end
