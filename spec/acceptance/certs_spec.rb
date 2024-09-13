theforeman/puppet-certs

commit 433dadc5ec41c2477fc6a04e056ca061fd818980

Copy the server CA certificate with file resource

manifests/ca.pp
@@ -12,10 +12,8 @@ class certs::ca (
   String $ca_expiration = $certs::ca_expiration,
   Boolean $generate = $certs::generate,
   Boolean $deploy = $certs::deploy,
-  Optional[Stdlib::Absolutepath] $server_cert = $certs::server_cert,
-  Optional[Stdlib::Absolutepath] $ssl_build_dir = $certs::ssl_build_dir,
-  String $group = $certs::group,
   String $owner = $certs::user,
+  String $group = $certs::group,
   Stdlib::Absolutepath $katello_server_ca_cert = $certs::katello_server_ca_cert,
   Stdlib::Absolutepath $ca_key = $certs::ca_key,
   Stdlib::Absolutepath $ca_cert = $certs::ca_cert,
@@ -23,6 +21,8 @@ class certs::ca (
   String $ca_key_password = $certs::ca_key_password,
   Stdlib::Absolutepath $ca_key_password_file = $certs::ca_key_password_file,
 ) {
+  $server_ca_path = "${certs::ssl_build_dir}/${server_ca_name}.crt"
+
   file { "${certs::pki_dir}/private/${default_ca_name}.pwd":
     ensure => absent,
   }
@@ -51,29 +51,29 @@ class certs::ca (
   }
   $default_ca = Ca[$default_ca_name]
 
-  if $server_cert {
-    ca { $server_ca_name:
-      ensure        => present,
-      generate      => $generate,
-      deploy        => false,
-      custom_pubkey => $certs::server_ca_cert,
-      build_dir     => $certs::ssl_build_dir,
+  if $certs::server_ca_cert {
+    file { $server_ca_path:
+      ensure => file,
+      source => $certs::server_ca_cert,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
     }
   } else {
-    ca { $server_ca_name:
-      ensure        => present,
-      generate      => $generate,
-      deploy        => false,
-      custom_pubkey => "${certs::ssl_build_dir}/${default_ca_name}.crt",
-      build_dir     => $certs::ssl_build_dir,
+    file { $server_ca_path:
+      ensure => file,
+      source => "${certs::ssl_build_dir}/${default_ca_name}.crt",
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
     }
   }
 
   if $generate {
-    file { "${ssl_build_dir}/KATELLO-TRUSTED-SSL-CERT":
+    file { "${certs::ssl_build_dir}/KATELLO-TRUSTED-SSL-CERT":
       ensure  => link,
-      target  => "${ssl_build_dir}/${server_ca_name}.crt",
-      require => Ca[$server_ca_name],
+      target  => $server_ca_path,
+      require => File[$server_ca_path],
     }
   }
 
@@ -94,7 +94,7 @@ class certs::ca (
 
     file { $katello_server_ca_cert:
       ensure => file,
-      source => "${certs::ssl_build_dir}/${server_ca_name}.crt",
+      source => $server_ca_path,
       owner  => $owner,
       group  => $group,
       mode   => '0644',

spec/acceptance/certs_spec.rb
@@ -124,4 +124,31 @@ describe 'certs' do
       it { should_not exist }
     end
   end
+
+  context 'with server CA cert' do
+    before(:context) do
+      source_path = "fixtures/example.partial.solutions-chain.pem"
+      dest_path = "/server-ca.crt"
+      scp_to(hosts, source_path, dest_path)
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+        class { 'certs':
+          server_ca_cert => '/server-ca.crt',
+        }
+        PUPPET
+      end
+    end
+
+    describe x509_certificate('/root/ssl-build/katello-server-ca.crt') do
+      it { should be_certificate }
+      # Doesn't have to be valid - can be expired since it's a static resource
+      it { should have_purpose 'CA' }
+      its(:issuer) { should eq('CN = Fake LE Root X1') }
+      its(:subject) { should eq('CN = Fake LE Intermediate X1') }
+      its(:keylength) { should be >= 2048 }
+    end
+  end
 end

commit 975bdb9611f58a5dbbf2ac8d3d764329507581f6

Fixes #37817: Only copy server CA in build root if generate is true
Fixes: 433dadc5ec41 ("Copy the server CA certificate with file resource")

manifests/ca.pp
@@ -45,25 +45,25 @@ class certs::ca (
     build_dir     => $certs::ssl_build_dir,
   }
 
-  if $certs::server_ca_cert {
-    file { $server_ca_path:
-      ensure => file,
-      source => $certs::server_ca_cert,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-    }
-  } else {
-    file { $server_ca_path:
-      ensure => file,
-      source => "${certs::ssl_build_dir}/${default_ca_name}.crt",
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-    }
-  }
-
   if $generate {
+    if $certs::server_ca_cert {
+      file { $server_ca_path:
+        ensure => file,
+        source => $certs::server_ca_cert,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+      }
+    } else {
+      file { $server_ca_path:
+        ensure => file,
+        source => "${certs::ssl_build_dir}/${default_ca_name}.crt",
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+      }
+    }
+
     file { "${certs::ssl_build_dir}/KATELLO-TRUSTED-SSL-CERT":
       ensure  => link,
       target  => $server_ca_path,

spec/acceptance/certs_spec.rb
@@ -151,4 +151,115 @@ describe 'certs' do
       its(:keylength) { should be >= 2048 }
     end
   end
+
+  context 'with tar file' do
+    before(:context) do
+      ['crt', 'key'].each do |ext|
+        source_path = "fixtures/example.partial.solutions.#{ext}"
+        dest_path = "/server.#{ext}"
+        scp_to(hosts, source_path, dest_path)
+      end
+    end
+
+    context 'with default ca' do
+      before(:context) do
+        manifest = <<~PUPPET
+          class { 'certs':
+            generate => true,
+            deploy   => false,
+          }
+
+          class { 'certs::foreman_proxy_content':
+            foreman_proxy_fqdn => 'foreman-proxy.example.com',
+            certs_tar          => '/root/foreman-proxy.example.com.tar.gz',
+          }
+        PUPPET
+
+        apply_manifest(manifest, catch_failures: true)
+
+        on default, 'rm -rf /root/ssl-build'
+      end
+
+      describe 'deploy certificates' do
+        manifest = <<-PUPPET
+          class { 'certs':
+            tar_file => '/root/foreman-proxy.example.com.tar.gz',
+          }
+        PUPPET
+        # tar extraction is not idempotent
+        it { apply_manifest(manifest, catch_failures: true) }
+      end
+
+      describe 'default and server ca certs match' do
+        it { expect(file('/etc/pki/katello/certs/katello-default-ca.crt').content).to eq(file('/etc/pki/katello/certs/katello-server-ca.crt').content) }
+      end
+
+      describe x509_certificate('/etc/pki/katello/certs/katello-default-ca.crt') do
+        it { should be_certificate }
+        it { should be_valid }
+        it { should have_purpose 'SSL server CA' }
+        its(:issuer) { should match_without_whitespace(/C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}/) }
+        its(:subject) { should match_without_whitespace(/C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}/) }
+        its(:keylength) { should be >= 4096 }
+      end
+    end
+
+    context 'with custom certificates' do
+      before(:context) do
+        manifest = <<~PUPPET
+          class { 'certs':
+            server_cert    => '/server.crt',
+            server_key     => '/server.key',
+            server_ca_cert => '/server-ca.crt',
+            generate       => true,
+            deploy         => false,
+          }
+
+          class { 'certs::foreman_proxy_content':
+            foreman_proxy_fqdn => 'foreman-proxy.example.com',
+            certs_tar          => '/root/foreman-proxy.example.com.tar.gz',
+          }
+        PUPPET
+
+        apply_manifest(manifest, catch_failures: true)
+
+        on default, 'rm -rf /root/ssl-build'
+      end
+
+      describe 'deploy certificates' do
+        manifest = <<-PUPPET
+          class { 'certs':
+            generate => false,
+            tar_file => '/root/foreman-proxy.example.com.tar.gz',
+          }
+        PUPPET
+        # tar extraction is not idempotent
+        it { apply_manifest(manifest, catch_failures: true) }
+      end
+
+      describe 'default and server ca certs match' do
+        it { expect(file('/etc/pki/katello/certs/katello-default-ca.crt').content).not_to eq(file('/etc/pki/katello/certs/katello-server-ca.crt').content) }
+      end
+
+      describe x509_certificate('/etc/pki/katello/certs/katello-default-ca.crt') do
+        it { should be_certificate }
+        it { should be_valid }
+        it { should have_purpose 'SSL server CA' }
+        its(:issuer) { should match_without_whitespace(/C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}/) }
+        its(:subject) { should match_without_whitespace(/C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}/) }
+        its(:keylength) { should be >= 4096 }
+      end
+
+      describe x509_certificate('/etc/pki/katello/certs/katello-server-ca.crt') do
+        it { should be_certificate }
+        it { should be_valid }
+        it { should have_purpose 'SSL server CA' }
+        # These don't match since we only configure it with the intermediate
+        # and not the actual root
+        its(:issuer) { should match_without_whitespace(/CN = Fake LE Root X1/) }
+        its(:subject) { should match_without_whitespace(/CN = Fake LE Intermediate X1/) }
+        its(:keylength) { should be >= 2048 }
+      end
+    end
+  end
 end