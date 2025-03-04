theforeman/puppet-certs

commit 028f93af283b718752e6263b2732c8c0ce308caf

Deploy CA directly from build directory

manifests/ca.pp
@@ -15,6 +15,7 @@ class certs::ca (
   Optional[Stdlib::Absolutepath] $server_cert = $certs::server_cert,
   Optional[Stdlib::Absolutepath] $ssl_build_dir = $certs::ssl_build_dir,
   String $group = $certs::group,
+  String $owner = $certs::user,
   Stdlib::Absolutepath $katello_server_ca_cert = $certs::katello_server_ca_cert,
   Stdlib::Absolutepath $ca_key = $certs::ca_key,
   Stdlib::Absolutepath $ca_cert = $certs::ca_cert,
@@ -41,79 +42,60 @@ class certs::ca (
     org_unit      => $org_unit,
     expiration    => $ca_expiration,
     generate      => $generate,
-    deploy        => $deploy,
+    deploy        => false,
     password_file => $ca_key_password_file,
     build_dir     => $certs::ssl_build_dir,
   }
   $default_ca = Ca[$default_ca_name]
 
+
   if $server_cert {
     ca { $server_ca_name:
       ensure        => present,
       generate      => $generate,
-      deploy        => $deploy,
+      deploy        => false,
       custom_pubkey => $certs::server_ca_cert,
       build_dir     => $certs::ssl_build_dir,
     }
   } else {
     ca { $server_ca_name:
-      ensure    => present,
-      generate  => $generate,
-      deploy    => $deploy,
-      ca        => $default_ca,
-      build_dir => $certs::ssl_build_dir,
+      ensure        => present,
+      generate      => $generate,
+      deploy        => false,
+      custom_pubkey => "${certs::ssl_build_dir}/${default_ca_name}.crt",
+      build_dir     => $certs::ssl_build_dir,
     }
   }
-  $server_ca = Ca[$server_ca_name]
 
   if $generate {
     file { "${ssl_build_dir}/KATELLO-TRUSTED-SSL-CERT":
       ensure  => link,
       target  => "${ssl_build_dir}/${server_ca_name}.crt",
-      require => $server_ca,
+      require => Ca[$server_ca_name],
     }
   }
 
   if $deploy {
-    Ca[$default_ca_name] ~>
-    pubkey { $ca_cert:
-      key_pair => $default_ca,
-    } ~>
-    pubkey { $ca_cert_stripped:
-      strip    => true,
-      key_pair => $default_ca,
-    } ~>
-    file { $ca_cert:
+    # Ensure CA key deployed to /etc/pki/katello/private no longer exists
+    # The CA key is not used by anything from this directory and does not need to be deployed
+    file { $ca_key:
+      ensure => absent,
+    }
+
+    file { $certs::katello_default_ca_cert:
       ensure => file,
+      source => "${certs::ssl_build_dir}/${default_ca_name}.crt",
       owner  => 'root',
-      group  => $group,
+      group  => 'root',
       mode   => '0644',
     }
 
-    Ca[$server_ca_name] ~>
-    pubkey { $katello_server_ca_cert:
-      key_pair => $server_ca,
-    } ~>
     file { $katello_server_ca_cert:
       ensure => file,
-      owner  => 'root',
+      source => "${certs::ssl_build_dir}/${server_ca_name}.crt",
+      owner  => $owner,
       group  => $group,
-      mode   => '0644',
-    }
-
-    if $generate {
-      Ca[$default_ca_name] ~>
-      privkey { $ca_key:
-        key_pair      => $default_ca,
-        unprotect     => true,
-        password_file => $ca_key_password_file,
-      } ~>
-      file { $ca_key:
-        ensure => file,
-        owner  => 'root',
-        group  => $group,
-        mode   => '0440',
-      }
+      mode   => '0440',
     }
   }
 }

manifests/init.pp
@@ -125,5 +125,4 @@ class certs (
   Class['certs::ca']
 
   $default_ca = $certs::ca::default_ca
-  $server_ca = $certs::ca::server_ca
 }

spec/acceptance/certs_spec.rb
@@ -14,7 +14,7 @@ describe 'certs' do
       it { is_expected.to be_installed }
     end
 
-    describe x509_certificate('/etc/pki/katello-certs-tools/certs/katello-default-ca.crt') do
+    describe x509_certificate('/root/ssl-build/katello-default-ca.crt') do
       it { should be_certificate }
       it { should be_valid }
       it { should have_purpose 'SSL server CA' }
@@ -23,13 +23,17 @@ describe 'certs' do
       its(:keylength) { should be >= 4096 }
     end
 
-    describe x509_certificate('/etc/pki/katello-certs-tools/certs/katello-server-ca.crt') do
+    describe x509_certificate('/root/ssl-build/katello-server-ca.crt') do
       it { should be_certificate }
       it { should be_valid }
       it { should have_purpose 'SSL server CA' }
       its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
       its(:subject) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
       its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key('/root/ssl-build/katello-default-ca.key') do
+      it { should be_encrypted }
     end
 
     describe x509_certificate('/etc/pki/katello/certs/katello-default-ca.crt') do
@@ -50,10 +54,66 @@ describe 'certs' do
       its(:keylength) { should be >= 4096 }
     end
 
-    describe x509_private_key('/etc/pki/katello/private/katello-default-ca.key') do
-      it { should_not be_encrypted }
+    describe file('/etc/pki/katello/private/katello-default-ca.key') do
+      it { should_not exist }
+    end
+
+    describe package("katello-default-ca") do
+      it { should_not be_installed }
+    end
+
+    describe package("katello-server-ca") do
+      it { should_not be_installed }
+    end
+  end
+
+  context 'with deploy false' do
+    before(:context) do
+      on default, 'rm -rf /root/ssl-build /etc/pki/katello'
+    end
+
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+          class { 'certs':
+            deploy => false
+          }
+        PUPPET
+      end
+    end
+
+    describe x509_certificate('/root/ssl-build/katello-default-ca.crt') do
+      it { should be_certificate }
       it { should be_valid }
-      it { should have_matching_certificate('/etc/pki/katello-certs-tools/certs/katello-default-ca.crt') }
+      it { should have_purpose 'SSL server CA' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_certificate('/root/ssl-build/katello-server-ca.crt') do
+      it { should be_certificate }
+      it { should be_valid }
+      it { should have_purpose 'SSL server CA' }
+      its(:issuer) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:subject) { should eq("C = US, ST = North Carolina, L = Raleigh, O = Katello, OU = SomeOrgUnit, CN = #{fact('fqdn')}") }
+      its(:keylength) { should be >= 4096 }
+    end
+
+    describe x509_private_key('/root/ssl-build/katello-default-ca.key') do
+      it { should be_encrypted }
+    end
+
+    describe file('/etc/pki/katello/certs/katello-default-ca.crt') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/pki/katello/certs/katello-server-ca.crt') do
+      it { should_not exist }
+    end
+
+    describe file('/etc/pki/katello/private/katello-default-ca.key') do
+      it { should_not exist }
     end
   end
 end

commit 328043dccd197ae0ebe3a567ec2ed96893dcc0e9

Remove unused ca_cert and ca_cert_stripped variables
Fixes: 028f93af283b ("Deploy CA directly from build directory")

manifests/ca.pp
@@ -15,8 +15,6 @@ class certs::ca (
   String $owner = $certs::user,
   String $group = $certs::group,
   Stdlib::Absolutepath $katello_server_ca_cert = $certs::katello_server_ca_cert,
-  Stdlib::Absolutepath $ca_cert = $certs::ca_cert,
-  Stdlib::Absolutepath $ca_cert_stripped = $certs::ca_cert_stripped,
   String $ca_key_password = $certs::ca_key_password,
   Stdlib::Absolutepath $ca_key_password_file = $certs::ca_key_password_file,
 ) {

manifests/init.pp
@@ -100,8 +100,6 @@ class certs (
     }
   }
 
-  $ca_cert = "${pki_dir}/certs/${default_ca_name}.crt"
-  $ca_cert_stripped = "${pki_dir}/certs/${default_ca_name}-stripped.crt"
   $ca_key_password = extlib::cache_data('foreman_cache_data', 'ca_key_password', extlib::random_password(24))
   $ca_key_password_file = "${ssl_build_dir}/${default_ca_name}.pwd"
 