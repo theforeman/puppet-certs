a0fc0fcd7815bc4830c07db0fbf01502ae666346
Tim Meusel <tim@bastelfreak.de>
2026-08-20 12:57:37 +0200

    modulesync 2026-08-20

--- spec/functions/validate_file_exists_spec.rb
+++ spec/functions/validate_file_exists_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'puppetlabs_spec_helper/puppetlabs_spec/puppet_internals'
 
 describe 'validate_file_exists' do
   it { should run.with_params('foo_doesnt_exist').and_raise_error(ArgumentError) }
